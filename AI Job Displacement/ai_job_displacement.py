370614e3b20b8754a5a4f3ecbacc9949d5bd3538
matthewcavanaugh96 <matthew.cavanaugh.96@gmail.com>
2026-03-03 16:18:45 -0800

    Getting py working

---
+++ AI Job Displacement/ai_job_displacement.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import streamlit as st
+import pandas as pd
+import unicodedata
+import streamlit as st
+from PIL import Image
+import random
+
+# --- Streamlit page setup ---
+st.set_page_config(page_title="Test", page_icon="🤖", layout="centered")
+
+st.title("⚾️ MLB statistics and machine learning")
+st.write("""
+Filler text. 
+
+""")
+
+
+st.title("🖥️ About the project")
+st.write("""
+More filler text.
+""")
+
+
